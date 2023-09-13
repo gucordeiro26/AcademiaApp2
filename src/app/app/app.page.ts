96678ef2a8e8b48a792c90bcb9fcca71d9fe7f3c
MartinsCaique <caiquety1@gmail.com>
2023-09-13 15:17:06 -0300

    Desing terminado da página de login e cadastro

--- src/app/app/app.page.ts
+++ src/app/app/app.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-app',
   templateUrl: './app.page.html',
@@ -10,6 +11,15 @@ export class AppPage implements OnInit {
     throw new Error('Method not implemented.');
   }
 
-  
+  isModalOpen = false;
 
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+
+  isModalOpenn = false;
+
+  setOpenn(isOpen: boolean) {
+    this.isModalOpenn = isOpen;
+  }
 }
