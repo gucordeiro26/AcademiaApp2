dccbd77dca8d5aa933481dcee7fa4d5034e99e2c
gucordeiro26 <gucordeiro26@gmail.com>
2023-09-20 16:07:14 -0300

    iniciando a aplicação funcional dos forms firebase

--- src/app/app/app.page.ts
+++ src/app/app/app.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { AuthenticateService } from '../services/auth.service';
+import { CrudService } from '../services/crud.service';
+import { Storage } from '@angular/fire/storage';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-app',
   templateUrl: './app.page.html',
   styleUrls: ['./app.page.scss'],
 })
-export class AppPage implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+
+export class AppPage {
+
+  constructor(
+    public _authenticate: AuthenticateService,
+    private _crudService: CrudService,
+    public storage: Storage,
+    private _message: MessageService,
+  ) {}
 
   isModalOpen = false;
 
@@ -21,5 +29,32 @@ export class AppPage implements OnInit {
 
   setOpenn(isOpen: boolean) {
     this.isModalOpenn = isOpen;
+  }
+
+  loginCliente = {
+    name: '',
+    email: '',
+    password: '',
+  }
+
+  loginClientes = [
+    {
+      name: this.loginCliente.name,
+      email: this.loginCliente.email,
+      password: this.loginCliente.password,
+    }
+  ]
+
+  cadastrarConta(dados: any){
+    this.loginCliente.name = dados.name;
+    this.loginCliente.email = dados.email;
+    this.loginCliente.password = dados.password;
+
+    this._authenticate.register(this.loginCliente.email, this.loginCliente.password);
+    this._crudService.insert(this.loginCliente, 'Cadastros no App');
+  }
+
+  realizarLogin(dados: any) {
+    this._authenticate.login(dados.name, dados.email);
   }
 }
