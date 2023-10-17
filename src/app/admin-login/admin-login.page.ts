c036a73fbc7cf05acb80312183c24258bed328e5
gucordeiro26 <gucordeiro26@gmail.com>
2023-10-17 18:02:44 -0300

    login admin funcionando lógica s/session

--- src/app/admin-login/admin-login.page.ts
+++ src/app/admin-login/admin-login.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.page.html',
   styleUrls: ['./admin-login.page.scss'],
 })
-export class AdminLoginPage implements OnInit {
+export class AdminLoginPage {
 
   constructor() { }
 
-  ngOnInit() {
+  isLoading = false;
+
+  loginAdmin(dados: any) {
+    this.isLoading = true;
+    
+    fetch('http://localhost/AcademiaAPP/admin/insert/authAdmin.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dados),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        this.isLoading = false;
+      })
   }
 
 }
